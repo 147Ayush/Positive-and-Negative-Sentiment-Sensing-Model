bc2f006ae3116242c683dfc279f8cc9c78f7801f
147Ayush <144997786+147Ayush@users.noreply.github.com>
2024-04-17 01:13:14 +0530

    Add files via upload

---
+++ nltkstopword.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+english_stopwords = set(stopwords.words('english'))
+
+with open('stopwords.txt', 'w') as file:
+    for word in english_stopwords:
+        file.write(word + '\n')
+
